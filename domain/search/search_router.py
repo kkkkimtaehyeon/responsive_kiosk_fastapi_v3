623defdaed1fe3508dae7f5785b15190e85548f3
kkkkimtaehyeon <kth1927@naver.com>
2024-05-10 16:31:12 +0900

    메뉴 검색 기능 추가

--- domain/search/search_router.py
+++ domain/search/search_router.py
@@ -13,16 +13,30 @@ class SearchKeywords(BaseModel):
 @router.post("")
 async def search(searchKeywords: SearchKeywords):
     search_script = generate_search_script(searchKeywords.ingredients)
-    return search_script
+
+    #received_menus = search_script로 gpt에서 메뉴 데이터 받아오는 함수
+
+    return dummy_data
 
 
 def generate_search_script(ingredients):
     ingredients = ', '.join(ingredients)
     return f'return menu infos include these keywords {ingredients}'
 
-dummy_data = {
-    "id": 1,
-    "name" : "americano",
-    "price": 3000,
-    "imagePath": "https://responsive-bucket.s3.ap-northeast-2.amazonaws.com/americano.jpg"
-}
+dummy_data = [
+    {
+        "id": 1,
+        "name": "아메리카노",
+        "price": 3000
+    },
+    {
+        "id": 2,
+        "name": "카페라떼",
+        "price": 3500
+    },
+    {
+        "id": 3,
+        "name": "바닐라라떼",
+        "price": 3700
+    }
+]
