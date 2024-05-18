a4db98ca712b69830c93dede515744207f2ac1fc
dldms401 <dldmsgkr401@gmail.com>
2024-05-19 01:19:19 +0900

    gpt-4o 추가

--- AI_domain/models/ai_key_bot.py
+++ AI_domain/models/ai_key_bot.py
@@ -4,7 +4,7 @@ from langchain.chains import LLMChain
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.memory import ConversationBufferMemory
 
-from .model_manage import KEYBOT
+from .model_manage import KEYBOT, GPT4O
 
 from dotenv import load_dotenv, find_dotenv
 import os
@@ -15,10 +15,10 @@ _ = load_dotenv(find_dotenv())
 llm = ChatOpenAI(
     api_key=os.getenv("OPENAI_API_KEY_EUNHAK"),
     
-    model_name= KEYBOT,
+    # model_name= KEYBOT,
+    model_name= GPT4O,
     
     tiktoken_model_name="gpt-3.5-turbo",
-    temperature=0.2,
     )
 
 # 기억 저장
@@ -30,10 +30,12 @@ memory = ConversationBufferMemory(
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        (f"system", """As a coffee shop bot responding to customer orders, 
-         if a sentence containing square brackets with keywords is input, please select all relevant items from the registered menus based on the keywords. 
-         Provide the ids in ascending order and format it into JSON. Items enclosed in parentheses signify menu registration. 
-         For any other input without brackets, handle the order conversationally as a human would.
+        ("system", """
+         Items enclosed in parentheses signify menu registration. 
+         ex) (id: 3, name: 딸기주스, price: 5500, description: 딸기를 갈아 넣은 주스, categoryName: 주스)
+         If a sentence containing square brackets with keywords is input, please select all relevant items from the registered menus based on the keywords, and then provide the IDs in ascending order and format it into JSON. 
+         ex) "menuList": ["id": 1,"name": "아메리카노","id": 2,"name": "라떼"]
+         When you gen answer, you don't need new lines, special characters, etc.
          """),
         MessagesPlaceholder(variable_name="history"),
         ("human", "{question}")
--- AI_domain/models/model_manage.py
+++ AI_domain/models/model_manage.py
@@ -3,3 +3,5 @@ CAFEBOT = "gpt-3.5-turbo"
 
 # 키워드에 알맞는 메뉴 id 리스트 JSON 생성
 KEYBOT = "ft:gpt-3.5-turbo-0125:personal:cafebot-keysearch:9Orl9xJQ"
+
+GPT4O = "gpt-4o"
