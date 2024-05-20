1fee19e7da0f972fa7685863372bf6983d137a62
kkkkimtaehyeon <kth1927@naver.com>
2024-05-21 04:58:46 +0900

    order 기능에 tts 추가

--- domain/ai_order/order_router.py
+++ domain/ai_order/order_router.py
@@ -1,12 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from ..common.user_script_dto import UserScript
 from domain.ai_order import order_service
+from AI_domain.functions.ai_order import order
+from domain.tts.openai_tts import text_to_speech
+from fastapi.responses import StreamingResponse
+from fastapi import HTTPException
+import os
 
 router = APIRouter(
     prefix="/fast/api/ai-order",
     tags=["ai-order"]
 )
 
+# @router.post("")
+# async def ai_order(userScript: UserScript):
+#     return order_service.order_with_ai(userScript.userScript)
+
+
 @router.post("")
 async def ai_order(userScript: UserScript):
-    return order_service.order_with_ai(userScript.userScript)
+    result = order(userScript.userScript)
+    result['gpt_audio_response'] = f"http://127.0.0.1:8000/fast/api/ai-order/audio?prompt={result['gpt_text_response']}"
+    return result
+
+@router.get("/audio")
+async def get_audio(prompt: str = Query(...)):
+    return await text_to_speech(prompt)
