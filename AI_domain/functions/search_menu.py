d0652cf4e4ef35bd57aaafa245b90a789223e281
dldms401 <dldmsgkr401@gmail.com>
2024-05-22 04:30:27 +0900

    search_menu 함수 비동기 처리

--- AI_domain/functions/search_menu.py
+++ AI_domain/functions/search_menu.py
@@ -5,7 +5,7 @@ from AI_domain.models.ai_key_bot import chain_keybot
 
 
 # ingredients 키워드 질문 / gpt 답변
-def search_menu(ingredients):
+async def search_menu(ingredients):
     menu_id_list = chain_keybot.predict(question = convert_to_searchform(ingredients))
 
     return convert_json(menu_id_list)
